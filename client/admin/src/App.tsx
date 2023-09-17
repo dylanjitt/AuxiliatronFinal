ab0a109a49c526d01d4782b428c7317cb0d16e2c
dylanJitton <dylanjitton2.0@gmail.com>
2023-09-16 22:39:04 -0400

    run build

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -1,7 +1,4 @@
 import "./App.css";
-import Component from "./components/TitleCard";
-import Pokedeck from './components/Pokedeck';
-import SumCalculator from "./components/CalculadoraSuma";
 import Calculadora from "./components/calculator/Calculadora";
 
 function App() {
